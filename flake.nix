730e2db8a623e88c445b1467249f44e0d2f394f2
Arpit Chakladar <54011232+arpitchakladar@users.noreply.github.com>
2025-07-02 22:48:37 +0530

    Updated flake.nix

--- flake.nix
+++ flake.nix
@@ -3,35 +3,38 @@
 
 	inputs = {
 		nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.05";
+		flake-utils.url = "github:numtide/flake-utils";
 	};
 
-	outputs = { self, nixpkgs }:
-	let
-		pkgs = nixpkgs.legacyPackages."x86_64-linux";
-	in {
-		devShells."x86_64-linux".default = (pkgs.buildFHSEnv {
-			name = "avalonia-dotnet-x11";
-			targetPkgs = pkgs: (with pkgs; [
-				dotnet-sdk_9
-				udev
-				alsa-lib
-				fontconfig
-				glew
-			]) ++ (with pkgs.xorg; [
-				# Avalonia UI
-				libX11
-				libICE
-				libSM
-				libXi
-				libXcursor
-				libXext
-				libXrandr
-			]);
-			multiPkgs = pkgs: (with pkgs; [
-				udev
-				alsa-lib
-			]);
-			runScript = "zsh";
-		}).env;
-	};
+	outputs = { self, nixpkgs, flake-utils }:
+		flake-utils.lib.eachDefaultSystem (system:
+			let
+				pkgs = import nixpkgs { inherit system; };
+			in {
+				devShells.default = (pkgs.buildFHSEnv {
+					name = "inkling-avalonia-dotnet";
+					targetPkgs = pkgs: (with pkgs; [
+						dotnet-sdk_9
+						udev
+						alsa-lib
+						fontconfig
+						glew
+					]) ++ (with pkgs.xorg; [
+						# Avalonia UI
+						libX11
+						libICE
+						libSM
+						libXi
+						libXcursor
+						libXext
+						libXrandr
+					]);
+					multiPkgs = pkgs: (with pkgs; [
+						udev
+						alsa-lib
+					]);
+					runScript = "zsh";
+				}).env;
+			}
+		);
 }
